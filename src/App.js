7df9122ef8d6e226c47727efa58fa97452b16ef0
fatima asebbane <fatimaasebbane2002@gmail.com>
2023-12-28 16:30:21 +0100

    version 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './pages/home'
 import { CallPepers } from './pages/CallPepers';
 import Contact from './pages/Contact';
 import committees from './pages/committees';
-
+import Registration from './components/Registration';
+import Submission from './components/Submission';
 function App() {
   return (
     <div>
@@ -14,8 +15,8 @@ function App() {
            <Route path='/call for papers' Component={CallPepers}/>
            <Route path='/Contact' Component={Contact}></Route>
            <Route path='/committees' Component={committees}></Route>
-
-
+           <Route path='/submission' Component={Submission}></Route>
+           <Route path='/Regstration' Component={Registration}></Route>
          </Routes>
       </Router>
     </div>
